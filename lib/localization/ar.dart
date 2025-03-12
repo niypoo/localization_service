c8c79f6f54f1fe806a9a57e129f72c49b36c2e16
Mahmoud Nabhan <niypoo@gmail.com>
2025-03-12 17:38:55 +0200

    add Language

--- lib/localization/ar.dart
+++ lib/localization/ar.dart
@@ -1,4 +1,5 @@
 const Map<String, String> localizationAr = {
   "LOCALIZATION.Languages":"اللغات",
+  "LOCALIZATION.Language":"اللغة",
   "LOCALIZATION.Change the locale of app to fit your knowledge.":"غيّر لغة التطبيق لتناسب معرفتك.",
 };
--- lib/localization/en.dart
+++ lib/localization/en.dart
@@ -1,5 +1,6 @@
 const Map<String, String> localizationEn = {
   "LOCALIZATION.Languages":"Languages",
+  "LOCALIZATION.Language":"Language",
   "LOCALIZATION.Change the locale of app to fit your knowledge.":"Change the locale of app to fit your knowledge.",
 
 };
